c3fd81e8e8abf3ac69235d0a8a1c22732d416d3c
DavidStevens1997 <david.hr97110105243@gmail.com>
2022-08-09 18:41:16 -0500

    segundo commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -15,3 +15,20 @@ module.exports = {
   BinarioADecimal,
   DecimalABinario,
 }
+
+/* function DecimalABinario(num =  76) {
+  // tu codigo aca
+  let binario = [];
+  let resto = 0;
+  let resultado = "";
+  while (num > 0) { 
+    resto = num%2;
+    num = Math.floor(num/2)
+     binario.push(resto);
+     console.log('Viendo el numero $(num) / 2 = $(num) resto $(resto)');
+  }
+console.log('Binario sinprocesar ${binario}');
+resultado = binario.reverse().join('');
+console.log('Resultado $resultado}')
+}
+DecimalABinario()*/
