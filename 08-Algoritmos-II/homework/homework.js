493eaa040ffc9806ec8812caa7edbfa6c64ee462
DavidStevens1997 <david.hr97110105243@gmail.com>
2022-08-28 14:18:15 -0500

    hm8

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,23 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length <= 1) {
+    return array;
+  }
+  let left = [];
+  let right = [];
+  let aux = [];
+  let pivot = array.pop();
+  let n = array.length;
+  for (let i = 0; i < n; i++) {
+    if (array[i] <= pivot){
+      left.push(array[i]);
+    }
+    else{
+      right.push(array[i]);
+    } 
+  }
+  return aux.concat(quickSort(left), pivot, quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +30,25 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+  }
 
+  const mitad = Math.floor(array.length / 2);
+  const left = array.slice(0,mitad)
+  const right = array.slice(mitad)
+  return margeTopDown(mergeSort(left), mergeSort(right));
+}
+function margeTopDown(left, right){
+  const array = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      array.push(left.shift())
+    } else{
+      array.push(right.shift())
+    }
+  }
+  return array.concat(left.slice()).concat(right.slice());
 }
 
 // No modificar nada debajo de esta línea
